aa0d46fabc5bdde8c06b49c60b12b42531416d21
s0ru <apazgarcia23@outlook.es>
2024-05-19 17:50:11 +0200

    add explosion animation basend on progress

--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -1,10 +1,19 @@
 uniform float uSize;
 uniform vec2 uResolution;
+uniform float uProgress;
 
 attribute float aSize;
 
+#include "../includes/remap.glsl";
+
 void main() {
-    vec4 modelPosition = modelMatrix * vec4(position, 1.0);
+    vec3 newPosition = position;
+
+    float explosionProgress = clamp(remap(uProgress, 0.0, 0.1, 0.0, 1.0), 0.0, 1.0);
+    explosionProgress = 1.0 - pow(1.0 - explosionProgress, 3.0);
+    newPosition = mix(vec3(0.0), newPosition, explosionProgress);
+
+    vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
 
     gl_Position = projectionMatrix * viewPosition;
