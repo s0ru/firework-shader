d8c0a3dba59c7b0a7a7c9fc41861a18f8b95e5cf
s0ru <apazgarcia23@outlook.es>
2024-05-19 17:56:56 +0200

    add resize animation based on progress

--- src/shaders/firework/vertex.glsl
+++ src/shaders/firework/vertex.glsl
@@ -17,10 +17,14 @@ void main() {
     fallingProgress = 1.0 - pow(1.0 - fallingProgress, 3.0);
     newPosition.y -= fallingProgress * 0.2;
 
+    float sizeProgressStart = remap(uProgress, 0.0, 0.125, 0.0, 1.0);
+    float sizeProgressEnd = remap(uProgress, 0.125, 1.0, 1.0, 0.0);
+    float sizeProgress = clamp(min(sizeProgressStart, sizeProgressEnd), 0.0, 1.0);
+
     vec4 modelPosition = modelMatrix * vec4(newPosition, 1.0);
     vec4 viewPosition = viewMatrix * modelPosition;
 
     gl_Position = projectionMatrix * viewPosition;
-    gl_PointSize = uSize * uResolution.y * aSize;
+    gl_PointSize = uSize * uResolution.y * aSize * sizeProgress;
     gl_PointSize *= 1.0 / -viewPosition.z;
 }
